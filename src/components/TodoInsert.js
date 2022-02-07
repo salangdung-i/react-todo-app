e6d4d3bf16d9536ed99e4855a0dc93a34d1c8512
salangdung-i <dladmswl92@naver.com>
2022-02-07 18:56:13 +0900

    컴포넌트 스타일링

---
+++ src/components/TodoInsert.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { MdCheckCircleOutline } from 'react-icons/md';
+import './TodoInsert.scss';
+
+const TodoInsert = () => {
+  return (
+    <form className="TodoInsert">
+      <input placeholder="할 일을 입력하세요." />
+      <button type="submit">
+        <MdCheckCircleOutline />
+      </button>
+    </form>
+  )
+};
+
+export default TodoInsert;
